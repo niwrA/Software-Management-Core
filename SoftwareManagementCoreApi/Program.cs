926284904e60e887e05e99759f2ae80f771cad7a
niwrA <arwin@niwra.nl>
2018-01-07 20:20:55 +0100

    Update Program to AspNetCore 2.0 preferred setup

--- SoftwareManagementCoreApi/Program.cs
+++ SoftwareManagementCoreApi/Program.cs
@@ -4,30 +4,30 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore;
 using Microsoft.Extensions.Configuration;
 
 namespace SoftwareManagementCoreApi
 {
     public class Program
     {
+
         static public IConfigurationRoot Configuration { get; set; }
+
         public static void Main(string[] args)
         {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .Build();
-
-            var builder = new ConfigurationBuilder()
-                 .SetBasePath(Directory.GetCurrentDirectory())
-                 .AddJsonFile("appsettings.json");
-
-            Configuration = builder.Build();
-
-            host.Run();
+            BuildWebHost(args).Run();
         }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .ConfigureAppConfiguration((hostContext, config) =>
+                {
+            // delete all default configuration providers
+            config.Sources.Clear();
+                    config.AddJsonFile("myconfig.json", optional: true);
+                })
+                .Build();
     }
 }
