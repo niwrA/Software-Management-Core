bd807d155a02830dc46c0a41dd5806c6a5de9531
Arwin van Arum <arwin.vanarum@cgm.com>
2018-01-14 12:01:01 +0100

    Begin setting up ProductInstallations domain and implementation

---
+++ ProductInstallationShared/ProductInstallations.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductInstallationShared
+{
+  public interface IProductInstallationState
+  {
+    Guid ProductGuid { get; set; }
+    Guid ProductVersionGuid { get; set; }
+    Guid CompanyGuid { get; set; }
+    Guid CompanyEnvironmentGuid { get; set; }
+  }
+  public interface IProductInstallationRepository
+  {
+
+  }
+  public class ProductInstallation
+  {
+    private IProductInstallationState _state;
+    private IProductInstallationRepository _repo;
+    public ProductInstallation(IProductInstallationState state, IProductInstallationRepository repo)
+    {
+      _state = state;
+      _repo = repo;
+    }
+    Guid ProductGuid { get { return _state.ProductGuid; } }
+    Guid ProductVersionGuid { get { return _state.ProductVersionGuid; } }
+    Guid CompanyGuid { get { return _state.CompanyGuid; } }
+    Guid CompanyEnvironmentGuid { get { return _state.CompanyEnvironmentGuid; } }
+  }
+    public class ProductInstallations
+    {
+
+    }
+}
